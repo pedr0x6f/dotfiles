69227c60c3d2734e8ef86cdc79bd3a3579c10150
Pedro Anderson <pedro.anderson@outlook.com.br>
2022-03-23 17:37:11 -0300

    Update install.sh

--- WSL2/install.sh
+++ WSL2/install.sh
@@ -49,7 +49,7 @@ echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> 
 
 # Instalar zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >>~/.zshrc
 
 # Instalar fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
