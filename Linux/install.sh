bcfe2a9a3475fd627812b452bb7c21f7854f490f
Pedro Anderson <pedro.anderson@outlook.com.br>
2022-04-06 14:29:24 -0300

    Update install.sh

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,25 +1,31 @@
 #! /bin/sh
 
-# remover bloqueios apt
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 
-# atualizacao dos repositorios e do sistema
 sudo apt update -y
 sudo apt full-upgrade -y
 
-# Download e instalacao nerd-fonts
-wget -c https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip  -P $HOME/Downloads/
+DOWNLOADS="$HOME/Downloads/programas"
+
+VSCODE="https://az764295.vo.msecnd.net/stable/e18005f0f1b33c29e81d732535d8c0e47cafb0b5/code_1.66.0-1648620611_amd64.deb"
+VIRTUALBOX="https://download.virtualbox.org/virtualbox/6.1.32/virtualbox-6.1_6.1.32-149290~Ubuntu~eoan_amd64.deb"
+EXTENSIONPACK="https://download.virtualbox.org/virtualbox/6.1.32/Oracle_VM_VirtualBox_Extension_Pack-6.1.32.vbox-extpack"
+
+wget -c https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip  -P "$DOWNLOADS"
 sudo unzip $HOME/Downloads/Hack.zip -d /usr/share/fonts
 fc-cache -fv
 
-# instalacao de programas
-sudo apt install keepassxc iotop htop preload vlc vim neovim git gcc g++ curl zsh unar unrar unzip p7zip p7zip-rar p7zip-full build-essential ubuntu-restricted-addons ubuntu-restricted-extras gufw fonts-hack-ttf fonts-firacode -y
+sudo apt install iotop htop preload vlc vim neovim git gcc g++ curl zsh unar unrar unzip p7zip p7zip-rar p7zip-full build-essential ubuntu-restricted-addons ubuntu-restricted-extras gufw fonts-hack-ttf fonts-firacode -y
 
-# habilitar firewall
+mkdir "$DOWNLOADS"
+wget -c "$VSCODE"        -P "$DOWNLOADS"
+wget -c "$VIRTUALBOX"    -P "$DOWNLOADS"
+wget -c "$EXTENSIONPACK" -P "$DOWNLOADS"
+sudo dpkg -i $DOWNLOADS/*.deb
+
 sudo ufw enable
 
-# configuracoes do sistema
 sudo tee -a /etc/sysctl.d/99-sysctl.conf <<-EOF
 vm.swappiness=1
 vm.vfs_cache_pressure=50
@@ -49,52 +55,9 @@ pedro soft nofile 8192
 pedro hard nofile 16384
 EOF
 
-# Powerlevel10K
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
-echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
-# zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-
-# zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >>~/.zshrc
-
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
-
-# nvm 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-# install latest lts version node
-nvm install --lts
-
-# update npm
-npm i npm -g
-
-# instalar vim-plug plugin manager
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Download arquivos de configuração   
-curl https://raw.githubusercontent.com/cezarmzz/dotfiles/main/.vimrc > ~/.vimrc
-
-# instalar vim-plug plugin manager
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-       
-# Download arquivos de configuração    
-sh -c 'curl -fLo ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/cezarmzz/dotfiles/40c3989bee6e5c9198895f6595e2f2b6b6b38f47/.config/nvim/init.vim'
-sh -c 'curl -fLo ~/.config/nvim/coc-settings.json --create-dirs https://raw.githubusercontent.com/cezarmzz/dotfiles/40c3989bee6e5c9198895f6595e2f2b6b6b38f47/.config/nvim/coc-settings.json'
-
-# atualizacao e limpeza do sistema
 sudo apt install -f -y
 sudo apt update -y
 sudo apt dist-upgrade -y
 sudo apt full-upgrade -y
-sudo apt install -f -y
 sudo apt autoclean -y
 sudo apt autoremove -y
